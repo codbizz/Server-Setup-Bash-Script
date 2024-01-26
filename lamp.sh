2646d3abed9ea1f3af01b3341ce145077edc5b6c
milan619 <tailormilan12@gmail.com>
2024-01-27 00:12:53 +0530

    remove mysql user

--- lamp.sh
+++ lamp.sh
@@ -15,7 +15,7 @@ sudo apt install mysql-server -y
 # Ask user if they want to run MySQL secure installation
 read -p "Do you want to secure your MySQL installation? (y/n): " SECURE_MYSQL
 
-if [ "$SECURE_MYSQL" == "y" ]; then
+if [ "$SECURE_MYSQL" = "y" ]; then
     sudo mysql_secure_installation
 fi
 
@@ -44,33 +44,14 @@ sudo apt install php$PHP_VERSION libapache2-mod-php$PHP_VERSION php$PHP_VERSION-
 # Ask if user wants to install Laravel required extensions
 read -p "Do you want to install Laravel required extensions? (y/n): " INSTALL_LARAVEL_EXTENSIONS
 
-if [ "$INSTALL_LARAVEL_EXTENSIONS" == "y" ]; then
+if [ "$INSTALL_LARAVEL_EXTENSIONS" = "y" ]; then
     echo "Installing Laravel required extensions..."
-    sudo apt install php$PHP_VERSION-curl php$PHP_VERSION-mbstring -y
+    sudo apt install php$PHP_VERSION-curl php$PHP_VERSION-mbstring php$PHP_VERSION-xml -y
 fi
 
 # Restart Apache web server
 echo "Restarting Apache web server..."
 sudo systemctl restart apache2
 
-# Ask user if they want to create a new database
-read -p "Do you want to create a new MySQL database? (y/n): " CREATE_DATABASE
 
-if [ "$CREATE_DATABASE" == "y" ]; then
-    # Prompt for database name and user credentials
-    read -p "Enter the new database name: " DB_NAME
-    read -p "Enter the database user: " DB_USER
-    read -sp "Enter the database user password: " DB_PASSWORD
-    echo # Move to a new line after password input
-
-    # Create the new database and user
-    sudo mysql -e "CREATE DATABASE $DB_NAME;"
-    sudo mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-    sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
-    sudo mysql -e "FLUSH PRIVILEGES"
-
-    echo "Database '$DB_NAME' and user '$DB_USER' created successfully."
-fi
-
-
-echo "Setup complete. You can now start your Laravel project. If you miss any required PHP extensions, install them using 'sudo apt install php$PHP_VERSION-extension_name'."
+echo "Setup complete. You can now start your Laravel project."
